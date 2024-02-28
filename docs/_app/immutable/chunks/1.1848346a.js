a75bfa93c0cc3a8436c6282f0f3c831fa9db9dc8
Chris Concannon <chris.concannon@sourcegraph.com>
2024-02-28 12:05:27 +0100

    add new examples

---
+++ docs/_app/immutable/chunks/1.1848346a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.56b87b6e.js";export{t as component};
--- docs/_app/immutable/chunks/1.8624b286.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.c551ae98.js";export{t as component};
